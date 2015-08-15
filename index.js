9e4d16875ddde2b0d9bc512d06efab56f44dfb0e
Fabricio C Zuardi <fabricio@fabricio.org>
2015-08-15 04:23:20 -0300

    newgame placeholder and debug of all messages

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-import serverConfig from './conf/server';
 import telegramConfig from './conf/telegram';
-
-import koa from "koa";
-import Router from 'koa-router';
-import requestbody from 'koa-body';
-const router = new Router();
-const koaBody = requestbody();
-
 import telegramBot from 'node-telegram-bot-api';
 
-// Setup polling way
-var bot = new telegramBot(telegramConfig.token, {polling: true});
-bot.on('text', function (msg) {
-  var chatId = msg.chat.id;
-  // photo can be: a file path, a stream or a Telegram file_id
+let bot = new telegramBot(telegramConfig.token, {polling: true});
 
-if (msg.text == '/love') {
-  var opts = {
-    reply_to_message_id: msg.message_id,
-    reply_markup: JSON.stringify({
-      keyboard: [
-        ['Yes, you are the bot of my life ❤'],
-        ['No, sorry there is another one...']]
-    })
-  };
-  bot.sendMessage(chatId, 'Do you love me?', opts);
-}
-    
-    
+let newGame = (chatId) => {
+    bot.sendMessage(chatId, 'Lets start a new game');
+};
+
+bot.on('text', function (msg) {
+    console.log(msg);
+
+    switch (msg.text) {
+        case '/newgame': 
+            newGame(msg.chat.id);
+            break;
+    }
 });
 
-// //webserver
-// let app = koa();
-
-// router.post('/' + telegramConfig.token, koaBody, function* webHook(next) {
-//     console.log('post received', this.request.body);
-//     yield next;
-// });
-// app.use(router.routes());
-
-// app.use(function* defaultResponse(){
-
-//     if (this.request.method == 'POST') {
-//         console.log(this.request.body);
-//         // => POST body
-//         this.body = JSON.stringify(this.request.body);
-//     }else{
-//         this.body = 'Hello World';
-//     }
-//     yield {};
-// });
-
-// console.log(`Server running on port ${serverConfig.port}`);
-// app.listen(serverConfig.port);
