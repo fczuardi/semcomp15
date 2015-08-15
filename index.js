4e01f9e75a6a98c424c5e0ddd6328df9d958bd9e
Fabricio C Zuardi <fabricio@fabricio.org>
2015-08-15 02:27:04 -0300

    basic webserver hello world on port 3030

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-import telegram from 'telegram-bot-api';
+import serverConfig from './conf/server';
 import telegramConfig from './conf/telegram';
 
-let api = new telegram({
+import koa from "koa";
+import telegram from 'telegram-bot-api';
+
+let telegramAPI = new telegram({
     token: telegramConfig.token
 });
 
-api.getMe( (err, data) => {
+//check if the robot exists
+telegramAPI.getMe( (err, data) => {
     console.log(err);
     console.log(data);
 });
+
+//webserver
+let app = koa();
+
+app.use(function* defaultResponse(){
+  this.body = 'Hello World';
+  yield {};
+});
+
+console.log(`Server running on port ${serverConfig.port}`);
+app.listen(serverConfig.port);
